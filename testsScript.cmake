eb49c44d4d3665ea5862a16c6d27efa69321b693
KosTTTT <kostttttttttt@gmail.com>
2024-09-17 15:39:45 +0300

    files moved+

---
+++ testsScript.cmake
@@ -0,0 +1,9 @@
+﻿set(tret 1)
+#fraction test
+message(STATUS "Running testsScript.cmake")
+execute_process(COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${var1}
+RESULT_VARIABLE tret
+)
+if(tret)
+    message(FATAL_ERROR "${var1} test failed.")
+endif()
